b5a1250ced13a0e847cdd1bbf749f80f914d8776
Takumi Ueda <takumi@ueda.dev>
2023-08-19 18:30:18 +0900

    Add setup.sh

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!bin/sh
+
+echo "Install .vimrc"
+ln -sf ~/git/github/uedatakumi/dotfiles/.vimrc ~/.vimrc
+
+echo "Install .tmux.conf"
+ln -sf ~/git/github/uedatakumi/dotfiles/.tmux.conf ~/.tmux.conf
